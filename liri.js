edbc90f0eac741d26c28644c727b0a74d47d70b4
pcruz0807 <codecruz86@gmail.com>
2019-03-03 19:45:59 -0800

    liri

--- liri.js
+++ liri.js
@@ -4,6 +4,21 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
+var request = require('request');
+
 var fs = require('fs');
 
 var moment = require('moment');
+
+
+shows = function(text) {
+request(request('https://rest.bandsintown.com/artists/' + text + '/events?app_id=codingbootcamp', function (err, response, body) {
+
+    var json = JSON.parse(body);
+    if (!json.length) {
+        console.log("Sorry could not find anything for " + text +
+            "\nShowing results for default concert");
+        searchThis('concert-this');
+    }
+})
+}
